b3c9a4e76657b8fee06765d9c7a3a2cd3fcf579a
aleena-joseph-86 <jaleena727@gmail.com>
2024-11-03 19:27:39 +0530

    Emotion detection and music recommendation is working perfectly.

--- app.py
+++ app.py
@@ -1,33 +1,31 @@
-from flask import Flask, render_template, Response, jsonify
-import gunicorn
-from camera import *
+from flask import Flask, render_template, Response
+from camera import VideoCamera  # Import your VideoCamera class
+import pandas as pd
 
 app = Flask(__name__)
+video_camera = VideoCamera()
 
-headings = ("Name","Album","Artist")
-df1 = music_rec()
-df1 = df1.head(15)
 @app.route('/')
 def index():
-    print(df1.to_json(orient='records'))
-    return render_template('index.html', headings=headings, data=df1)
+    # Render the index page
+    return render_template('index.html')
 
-def gen(camera):
+def generate_frames():
     while True:
-        global df1
-        frame, df1 = camera.get_frame()
+        frame, df1 = video_camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/t')
-def gen_table():
-    return df1.to_json(orient='records')
+@app.route('/music_recommendations')
+def music_recommendations():
+    # Retrieve the current DataFrame from the video camera
+    _, df1 = video_camera.get_frame()
+    # Convert DataFrame to HTML for rendering in the index page
+    return df1.to_html(classes='table table-striped', index=False)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug=True)
